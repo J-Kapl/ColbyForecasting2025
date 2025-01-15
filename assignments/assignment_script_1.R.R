dc531d8a20e000e2ad40ecdee3a3856e48d93dec
Joshua M Kaplan <joshkap@Joshuas-MacBook-Pro.local>
2025-01-15 15:15:11 -0500

    update assignment 1

--- assignments/assignment_script_1.R.R
+++ assignments/assignment_script_1.R.R
@@ -1,9 +1,16 @@
+# Filter for our scenario
 db = DB |>
      dplyr::filter(scenario == "RCP45", year == 2055, interval == "mon")
 x = read_brickman(db)
 x
 
+# Extract SST data from the buoys
 long_values = dplyr::filter(long_values, name == "SST")
 
+# Filter to buoy M01
 M01 = long_values |> filter(point == "p5")
+
+# Plot SST by month
 ggplot(data = M01, mapping = aes(x = month, y = value)) + geom_point()
+
+# Get the months in order
