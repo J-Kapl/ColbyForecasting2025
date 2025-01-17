a445c64a26b4046b4d44ae2a0cd19aa35c2a21a4
Joshua M Kaplan <joshkap@Joshuas-MacBook-Pro.local>
2025-01-17 15:57:48 -0500

    add S02 and model_input.R

---
+++ functions/model_input.R
@@ -0,0 +1,18 @@
+#' Reads a model input file given species, month, approach and path
+#' 
+#' @param scientificname chr, the species name
+#' @param mon chr month abbreviation
+#' @param approach chr, one of "greedy" or "conservative"
+#' @param path chr the path to the data directory
+read_model_input = function(scientificname = "Xiphias gladius",
+                            mon = "Aug",
+                            approach = "greedy",
+                            path = data_path("model_input")){
+  # your part goes in here
+  
+  # save the data
+  filename = sprintf("%s-%s-%s_input.gpkg", 
+                     gsub(" ", "_", species),
+                     mon, approach)
+  read_sf(file.path(path, filename))
+}
